1690189c4961fb4bc74a3d05342cdddf72e05802
Manideepsai <manideepsai06@gmail.com>
2026-06-10 12:14:08 +0530

    initial commit: fetcher.py done

--- fetch.py
+++ fetch.py
@@ -1,31 +1,22 @@
-import requests
+import requests 
+import time
 
-#getting coins data to get required data from them
-url = "https://api.coinpaprika.com/v1/coins"
-response = requests.get(url)
-coins=response.json()
-
-#symbols which we need to get id from coin data from api
-symb=["BTC", "ETH", "BNB", "SOL", "XRP"] 
-
-#getting required ids from cons data
-ids=[]
-for i in coins:
-  if(i["symbol"] in symb):
-    ids.append(i["id"])
-
-
-#function to get coins data using coinpaprika api
-def fetch1(coinname):
-  url = "https://api.coinpaprika.com/v1/tickers/"+coinname
-  resp=requests.get(url)
-  if resp.status_code == 404:
+def fetch_data():
+  url = "https://api.coinpaprika.com/v1/tickers"
+  try:
+    response = requests.get(url).json()
+  except Exception as e:
+    print(f"Something went wrong: {e}")
     return None
-  fin=resp.json()["quotes"]["USD"]["price"]
-  print(f"the price of {coinname} is {fin:0.0f}")
 
-for i in ids:
-  fetch1(i)
+  dictonary=dict()
 
+  lis=["BTC", "ETH", "BNB", "SOL", "XRP"]
 
-  
+  for i in range(len(response)):
+    if(response[i]["symbol"] in lis):
+      print(response[i]["symbol"])
+      dictonary[response[i]["symbol"]]=response[i]["quotes"]["USD"]["price"]
+  return dictonary
+
+print(fetch_data())
