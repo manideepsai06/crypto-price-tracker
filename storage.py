e72617f0969fa0b7e37cb2b908477e73191897d5
Manideepsai <manideepsai06@gmail.com>
2026-07-10 11:54:57 +0530

    added storage function through sqLite

--- storage.py
+++ storage.py
@@ -1,17 +1,32 @@
-from transform import transform_data
 import sqlite3
-conn = sqlite3.connect("crypto.db")  # creates file if doesn't exist
-cur=conn.cursor()
+processed_list=[{"symbol":"BTC","Price":100000,"Timestamp":"2026-01-01 00:00:00","pct":10},{"symbol":"ETH","Price":20000,"Timestamp":"2026-01-01 00:00:00","pct":20}]
 
 def init_db():
-  n=transform_data()
+  conn = sqlite3.connect("crypto.db")  # creates file if doesn't exist
+  cur=conn.cursor() #cursor to execute commands
+
+  #create table if not exists
   cur.execute("CREATE TABLE IF NOT EXISTS cryptoPrices(symbol,price,timestamp,pct)")
-  data=(n[0]["symbol"],n[0]["Price"],n[0]["Timestamp"],n[0]["pct"])
-  cur.execute("INSERT INTO cryptoPrices VALUES(?,?,?,?)",data)
   conn.commit()
+  conn.close()
+  return None
+
+def store_data(processed_list):
+  conn = sqlite3.connect("crypto.db")  # creates file if doesn't exist
+  cur=conn.cursor() #cursor to execute commands
+
+  for item in processed_list:
+    cur.execute("INSERT INTO cryptoPrices VALUES(?,?,?,?)",(item["symbol"],item["Price"],item["Timestamp"],item["pct"]))
   cur.execute("SELECT * FROM cryptoPrices")
   res=cur.fetchall()
+  print(res)
+  conn.commit()
   conn.close()
-  return res
+
+  return None
+
+
 if __name__=="__main__":
-  print(init_db())
+  init_db()
+  store_data(processed_list)
+
